94835e69b9d007173e4d9d1e2f4bf0582b86d921
Pascal Hartig <phartig@twitter.com>
2015-09-10 15:39:56 +0200

    Migrate example test to Kotlin

---
+++ app/src/test/kotlin/net/rdrei/savehtml/extractor/HtmlExtractorTest.kt
@@ -0,0 +1,12 @@
+package net.rdrei.savehtml.extractor
+
+import org.junit.Test
+import org.assertj.core.api.Assertions.*
+
+
+public class HtmlExtractorTest {
+    Test
+    public fun testSomething() {
+        assertThat("hello").endsWith("allo")
+    }
+}
