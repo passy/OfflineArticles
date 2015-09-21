d6b6521e444988973483f0c78a8bbc5340ab88ae
Pascal Hartig <phartig@twitter.com>
2015-09-21 21:21:46 +0100

    Another CSS parser, new problems

--- app/src/main/kotlin/net/rdrei/savehtml/extractor/CssExtractor.kt
+++ app/src/main/kotlin/net/rdrei/savehtml/extractor/CssExtractor.kt
@@ -1,18 +1,13 @@
 package net.rdrei.savehtml.extractor
 
-import com.osbcp.cssparser.CSSParser
-import com.osbcp.cssparser.Rule
+import com.helger.css.decl.CascadingStyleSheet
 import java.util.*
-
-data class StyleSheet(val data: String)
 
 /**
  * A non-recursive extractor for CSS resources.
  */
 public object CssExtractor {
-    public fun extract(document: StyleSheet): Resources {
-        val rules: List<Rule> = CSSParser.parse(document.data)
-
+    public fun extract(document: CascadingStyleSheet): Resources {
         return Resources(Collections.emptySet(), Collections.emptySet())
     }
 }
--- app/src/test/java/net/rdrei/savehtml/extractor/CssExtractorTest.kt
+++ app/src/test/java/net/rdrei/savehtml/extractor/CssExtractorTest.kt
@@ -1,13 +1,17 @@
 package net.rdrei.savehtml.extractor
 
+import com.helger.commons.charset.CCharset
+import com.helger.css.ECSSVersion
+import com.helger.css.reader.CSSReader
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 public class CssExtractorTest {
     @Test
     public fun testUnidirectionalExtraction() {
-        val input = StyleSheet(TestUtil.openStringResource("net/rdrei/savehtml/extractor/css0/styles.css"))
-        val resources = CssExtractor.extract(input)
+        val load = { -> TestUtil.openResource("net/rdrei/savehtml/extractor/css1/styles.css") }
+        val css = CSSReader.readFromStream(load, CCharset.CHARSET_UTF_8_OBJ, ECSSVersion.LATEST)
+        val resources = CssExtractor.extract(css)
 
         // TODO
         assertThat(resources.styles).hasSize(0)
